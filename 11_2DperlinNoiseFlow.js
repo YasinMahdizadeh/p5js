c9337c3c98972b7da8803c9734a638c5694bcb2d
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2024-05-05 11:28:37 +0330

    Initiation

--- 11_2DperlinNoiseFlow.js
+++ 11_2DperlinNoiseFlow.js
@@ -1,28 +1,33 @@
-var xoff = 0.00;
 var inc = 0.03;
+var scl = 20;
+var cols, rows;
 
 function setup() {
     createCanvas(1080,1080);
     frameRate(30);
-    
+    cols = floor(width / scl);
+    rows = floor(height / scl);
+
 }
 
 function draw() {
     background(20);
-    loadPixels();
-    for ( x = 0 ; x < width; x++) {
-        var yoff = 0.00;
+    var yoff = 0.00;
 
-        for ( y = 0; y < height; y++){
+    loadPixels();
+    for ( y = 0 ; y < height; y++) {
+        var xoff = 0.00;
+
+        for ( x = 0; x < width; x++){
             var index = ( x + y * width ) * 4;
             var r = noise(xoff,yoff) * 255;
             pixels[index + 0] = r;
             pixels[index + 1] = r;
             pixels[index + 2] = r;
             pixels[index + 3] = 255;
-            yoff += inc;
+            xoff += inc;
         }
-        xoff += inc;
+        yoff += inc;
     }
     updatePixels();
     noLoop();
