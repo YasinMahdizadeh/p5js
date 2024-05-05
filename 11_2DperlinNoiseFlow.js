e98c65a83e2bed6a48ed7676e5d2dd0a9600e4b3
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2024-05-05 11:44:01 +0330

    draw vectors

--- 11_2DperlinNoiseFlow.js
+++ 11_2DperlinNoiseFlow.js
@@ -2,12 +2,15 @@ var inc = 0.2;
 var scl = 20;
 var cols, rows;
 
+var fr;
+
 function setup() {
     createCanvas(400,400);
     frameRate(30);
     cols = floor(width / scl);
     rows = floor(height / scl);
-
+    //frameRate Parameter
+    fr = createP();
 }
 
 function draw() {
@@ -20,11 +23,19 @@ function draw() {
         for ( x = 0; x < rows; x++){
             var index = ( x + y * width ) * 4;
             var r = noise(xoff,yoff) * 255;
-            fill(r);
-            rect(x * scl, y * scl, scl, scl);
+            var v = p5.Vector.fromAngle(0);
+            // fill(r);
+            // rect(x * scl, y * scl, scl, scl);
+            stroke(185);
+            push();
+            translate(x * scl, y * scl);
+            rotate(v.heading());
+            line(0, 0, scl, 0);
+            pop();
             xoff += inc;
         }
         yoff += inc;
     }
-   
+   //Shows frameRates
+    fr.html(floor(frameRate()));
 }
